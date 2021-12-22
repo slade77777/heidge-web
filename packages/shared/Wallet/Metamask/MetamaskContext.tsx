d87edab84364a15f569f6ec1b377dfcc417c72f2
dungho <dung.ho@onemount.com>
2021-12-22 10:15:30 +0700

    fix window dom

--- packages/shared/Wallet/Metamask/MetamaskContext.tsx
+++ packages/shared/Wallet/Metamask/MetamaskContext.tsx
@@ -1,17 +1,84 @@
-import { ReactNode, createContext } from "react";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useRef,
+  useState,
+  useEffect,
+} from 'react';
+import MetaMaskOnboarding from '@metamask/onboarding';
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+
+const ONBOARD_TEXT = 'Click here to install MetaMask!';
+const CONNECT_TEXT = 'Connect';
+const CONNECTED_TEXT = 'Connected';
 
 type ContextType = {
   connect?: () => void;
+  disabled?: boolean;
 };
 const Context = createContext<ContextType>({});
 
 const MetamaskProvider = ({ children }: { children: ReactNode }) => {
-  function connect() {}
+  const [buttonText, setButtonText] = useState(ONBOARD_TEXT);
+  const [isDisabled, setDisabled] = useState(false);
+  const [accounts, setAccounts] = useState<string[]>([]);
+  const onboarding = useRef<MetaMaskOnboarding>();
+
+  useEffect(() => {
+    if (!onboarding.current) {
+      onboarding.current = new MetaMaskOnboarding();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      if (accounts.length > 0) {
+        setButtonText(CONNECTED_TEXT);
+        setDisabled(true);
+        onboarding.current?.stopOnboarding();
+      } else {
+        setButtonText(CONNECT_TEXT);
+        setDisabled(false);
+      }
+    }
+  }, [accounts]);
+
+  useEffect(() => {
+    function handleNewAccounts(newAccounts: string[]) {
+      setAccounts(newAccounts);
+    }
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      window.ethereum
+        .request({ method: 'eth_requestAccounts' })
+        .then(handleNewAccounts);
+      window.ethereum.on('accountsChanged', handleNewAccounts);
+      return () => {
+        window.ethereum.removeListener('accountsChanged', handleNewAccounts);
+      };
+    }
+  }, []);
+
+  function connect() {
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      window.ethereum
+        .request({ method: 'eth_requestAccounts' })
+        .then((newAccounts: string[]) => setAccounts(newAccounts));
+    } else {
+      onboarding.current?.startOnboarding();
+    }
+  }
 
   return (
     <Context.Provider
       value={{
         connect,
+        disabled: isDisabled,
       }}
     >
       {children}
@@ -19,4 +86,6 @@ const MetamaskProvider = ({ children }: { children: ReactNode }) => {
   );
 };
 
-export default MetamaskProvider;
+const useMetamask = () => useContext(Context);
+
+export { MetamaskProvider, useMetamask };
--- packages/shared/components/Button.tsx
+++ packages/shared/components/Button.tsx
@@ -1,6 +1,6 @@
 import Spinner from './Spinner';
 import { ReactNode } from 'react';
-import {classNames} from "../utils";
+import { classNames } from '../utils';
 
 type Props = {
   disabled?: boolean;
@@ -15,7 +15,7 @@ const Button = ({ disabled, loading, children, onClick, className }: Props) => {
     <button
       type="button"
       onClick={onClick}
-      className={classNames(className, loading ? 'cursor-not-allowed' : '')}
+      className={classNames(className, loading ? 'cursor-not-allowed' : 'btn')}
       disabled={loading || disabled}
     >
       {loading && <Spinner />}
