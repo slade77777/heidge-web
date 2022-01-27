a0947c3105d445c9c1f30f20eb2872161617922e
dungho <chris.tridung@gmail.com>
2022-01-27 14:04:10 +0700

    updating footer

--- apps/landing/components/Footer.tsx
+++ apps/landing/components/Footer.tsx
@@ -3,8 +3,8 @@ import { Channels, NavLinks } from './Header';
 const Footer = () => {
   return (
     <footer className="text-center text-fuchsia-600 bg-hdgBlack h-44 py-12">
-      <div className="flex flex-col items-center space-y-4 mx-auto max-w-md">
-        <NavLinks className="font-semibold grid grid-cols-3 divide-x divide-fuchsia-900" />
+      <div className="flex flex-col items-center space-y-4 mx-auto max-w-lg">
+        <NavLinks className="font-semibold hidden md:grid grid-cols-4 divide-x divide-fuchsia-900" />
         <Channels className="flex space-x-3" />
         <span className="text-xs">
           © Copyright 2019 Hedgie Inc. All rights reserved
