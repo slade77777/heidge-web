7f0408e451e8a6e0baddbaa37cee97fb80be14a6
dungho <chris.tridung@gmail.com>
2022-02-09 10:40:56 +0700

    change text

--- apps/landing/components/BuyYourHedgie.tsx
+++ apps/landing/components/BuyYourHedgie.tsx
@@ -7,7 +7,7 @@ const BuyYourHedgie = () => {
   }
   return (
     <div className="text-center bg-slate-100 py-10">
-      <h4 className="text-fuchsia-900 font-semibold py-3">Are you ready?</h4>
+      <h4 className="text-fuchsia-900 font-semibold py-3">Ready?</h4>
       <Button
         onClick={scrollToMintButton}
         className="btn btn-cyan uppercase px-8 leading-5"
