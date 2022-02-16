e7d265c3076869c26e7ad023e1bed4fbc99a1500
dungho <chris.tridung@gmail.com>
2022-02-16 20:07:08 +0700

    update check page

--- apps/landing/components/HowToFindHexCode.tsx
+++ apps/landing/components/HowToFindHexCode.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { OPENSEA_URL } from '../constants';
+import Image from 'next/image';
 
 const HowToFindHexCode = () => {
   return (
-    <div className="prose lg:prose-lg rounded bg-slate-50 p-4 text-gray-600 prose-a:text-teal-400 prose-a:hover:text-fuchsia-900 prose-a:hover:cursor-pointer">
+    <div className="prose mt-3 lg:prose-lg border rounded shadow-md p-4 prose-a:text-teal-400 prose-a:hover:text-fuchsia-900 prose-a:hover:cursor-pointer">
       <p>
         On{' '}
         <a href={OPENSEA_URL} target="_blank" rel="noreferrer">
@@ -12,12 +13,33 @@ const HowToFindHexCode = () => {
         on a given Hedgie detail page you will see hex code like so. Copy if,
         and paste into the field above.
       </p>
+      <Image
+        src="/images/hex-code.png"
+        width={716}
+        height={280}
+        objectFit="contain"
+        alt="hex-code"
+      />
       <p>
         Certain special edition Hedgies will not have the hex code listed on
         OpenSea. In this case, click on the website link and go to that Hedgie’s
         page on Hedgie website.
       </p>
+      <Image
+        src="/images/special-hex-code.png"
+        width={710}
+        height={280}
+        alt="special-hex-code"
+        objectFit="contain"
+      />
       <p>Copy the hex code as shown, and paste it into the field above.</p>
+      <Image
+        src="/images/special-hedgie-site.png"
+        width={710}
+        height={280}
+        objectFit="contain"
+        alt="special-hedgie-site"
+      />
     </div>
   );
 };
