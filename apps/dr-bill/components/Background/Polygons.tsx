ffabbd8814fb20b95cdd9a8dabf951734453d04b
binhnd <binh.nguyen3@onemount.com>
2022-03-04 18:03:01 +0700

    add polygons to background

--- apps/dr-bill/components/Background/Polygons.tsx
+++ apps/dr-bill/components/Background/Polygons.tsx
@@ -1,7 +1,32 @@
-export const Triangle1 = () => {
+export const TriangleAndSquare = ({className}) => {
   return (
-    <svg width="519" height="395" viewBox="0 0 519 395" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path opacity="0.03" d="M379.576 394.7L0.281788 15.1627L518.286 -123.636L379.576 394.7Z" fill="black"/>
+    <svg className={className} width="519" height="395" viewBox="0 0 519 395" fill="none"
+         xmlns="http://www.w3.org/2000/svg">
+      <path opacity="0.03" d="M379.576 394.7L0.281777 15.1627L518.286 -123.636L379.576 394.7Z" fill="currentColor"/>
+      <rect opacity="0.03" x="358" y="22.5703" width="125.138" height="125.138" transform="rotate(-30 358 22.5703)"
+            fill="currentColor"/>
+    </svg>
+  )
+}
+
+export const Triangle = ({className}) => {
+  return (
+    <svg className={className} width="406" height="422" viewBox="0 0 406 422" fill="none"
+         xmlns="http://www.w3.org/2000/svg">
+      <path opacity="0.03" d="M96.8214 421.91L-16.0839 0.000710053L405.555 112.978L96.8214 421.91Z" fill="black"/>
+    </svg>
+
+  )
+}
+
+export const PolygonAndEllipse = ({className}) => {
+  return (
+    <svg className={className} width="219" height="219" viewBox="0 0 219 219" fill="none"
+         xmlns="http://www.w3.org/2000/svg">
+      <path opacity="0.03"
+            d="M89.0984 0.232014L218.716 47.5795L210.246 182.655L75.3929 218.789L0.519622 106.045L89.0984 0.232014Z"
+            fill="currentColor"/>
+      <circle opacity="0.03" cx="171.5" cy="36.5" r="28.5" fill="currentColor"/>
     </svg>
   )
 }
--- apps/dr-bill/components/Background/index.tsx
+++ apps/dr-bill/components/Background/index.tsx
@@ -1,10 +1,11 @@
-import {Text} from '@nextui-org/react';
-import {Triangle1} from './Polygons';
+import {PolygonAndEllipse, Triangle, TriangleAndSquare} from './Polygons';
 
 export default function Background({children}) {
   return (
     <div className="relative">
-      <Triangle1/>
+      <TriangleAndSquare className="absolute right-10"/>
+      <PolygonAndEllipse className="absolute top-2/3 right-1/2"/>
+      <Triangle className="absolute top-1/2"/>
       {children}
     </div>
   )
