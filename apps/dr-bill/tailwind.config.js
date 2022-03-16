d29147a52d94480c5733c5f206467ad4a5008311
dungho <chris.tridung@gmail.com>
2022-03-16 14:52:28 +0700

    get rid of form plugin

--- apps/dr-bill/tailwind.config.js
+++ apps/dr-bill/tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
   extend: {},
   plugins: [
     require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/forms'),
+    // require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
   ],
 };
