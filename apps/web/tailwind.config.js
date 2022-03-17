964e12fb5d716451bca376a2615a4676e7717330
sonth <sonth2@onemount.com>
2022-03-18 00:54:38 +0700

    leader board page

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,3 +1,17 @@
+const colors = {
+  h_cyan: {
+    100: "#12dfc0",
+  },
+  h_yellow: {
+    coin: "#ffe900",
+  },
+  rank: {
+    1: "#ffb129",
+    2: "#ff48c1",
+    3: "#009cde",
+  },
+};
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -7,6 +21,10 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Raleway", "sans-serif"],
+      },
+      colors: colors,
+      boxShadow: {
+        100: "rgb(0 0 0 / 26%) 0px 0px 8px 0px",
       },
     },
   },
