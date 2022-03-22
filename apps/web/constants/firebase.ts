2efff608ce33cb2c1ca4adf2dac39e0f55c47cf2
dungho <chris.tridung@gmail.com>
2022-03-22 09:15:25 +0700

    [web] refactor firebase #4

--- apps/web/constants/firebase.ts
+++ apps/web/constants/firebase.ts
@@ -1,12 +1,19 @@
 import type { FireBaseConfig } from "shared";
 
 export const firebaseConfig: FireBaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL || "",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
+  apiKey:
+    process.env.NEXT_PUBLIC_FIREBASE_API_KEY ||
+    "AIzaSyC6bX4CJiSIA_Wwxp5pHVYIgaNv5BS7Reo",
+  authDomain:
+    process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ||
+    "hedgie-47de7.firebaseapp.com",
+  databaseURL:
+    process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL ||
+    "https://hedgie-47de7.firebaseio.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "hedgie-47de7",
   storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.appspot.com`,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
+  messagingSenderId:
+    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "1059513233216",
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
   measurementId: "G-MEASUREMENT_ID",
 };
