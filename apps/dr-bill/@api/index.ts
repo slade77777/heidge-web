463557c1a984896cf3532ccc852a23c94a0d7b86
Chris Ho <chris.tridung@gmail.com>
2022-03-29 10:25:04 +0700

    [dr-bill] api integration #6 change to private url

--- apps/dr-bill/@api/index.ts
+++ apps/dr-bill/@api/index.ts
@@ -1,29 +1,31 @@
 import { Content } from '../types';
 
-const baseUrl = process.env.API_URL;
+const ssrBaseUrl = process.env.API_URL;
 
 export function getHomeContents(): Promise<Content[]> {
-  return fetch(`${baseUrl}/content/home/0`).then((res) => res.json());
+  return fetch(`${ssrBaseUrl}/content/home/0`).then((res) => res.json());
 }
 
 export function getArtworkByIndex(index: number) {
-  return fetch(`${baseUrl}/content/artworks/${index}`).then((res) =>
+  return fetch(`${ssrBaseUrl}/content/artworks/${index}`).then((res) =>
     res.json(),
   );
 }
 
 export function getArtworkCategoryByIndex(index: number, category: string) {
-  return fetch(`${baseUrl}/content/${category}/${index}`).then((res) =>
+  return fetch(`${ssrBaseUrl}/content/${category}/${index}`).then((res) =>
     res.json(),
   );
 }
 
 export function getArtworkBySlug(slug: string) {
-  return fetch(`${baseUrl}/content/artworks/${slug}`).then((res) => res.json());
+  return fetch(`${ssrBaseUrl}/content/artworks/${slug}`).then((res) =>
+    res.json(),
+  );
 }
 
 export function getShowAndMedia(index: number) {
-  return fetch(`${baseUrl}/content/show-and-media/${index}`).then((res) =>
+  return fetch(`${ssrBaseUrl}/content/show-and-media/${index}`).then((res) =>
     res.json(),
   );
 }
