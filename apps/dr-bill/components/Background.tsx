58886d8f60f359dde7fd4d6e0f6817863f41a540
Chris Ho <chris.tridung@gmail.com>
2022-04-07 20:32:39 +0700

    [dr-bill] fix light blur bg

--- apps/dr-bill/components/Background.tsx
+++ apps/dr-bill/components/Background.tsx
@@ -3,26 +3,48 @@ import Polygon from './Polygon';
 
 const Polygons = () => (
   <div className="absolute top-0 w-full h-full overflow-hidden">
-    <Polygon size={300} className="triangle absolute top-0 right-0 rotate-[165deg] opacity-5"/>
-    <Polygon size={380} className="triangle absolute top-[950px] -left-10 rotate-[195deg] opacity-5"/>
-    <Polygon size={100} className="square absolute top-0 right-6 rotate-[60deg] opacity-5"/>
-    <Polygon size={87} className="square absolute top-[2200px] left-10 rotate-[30deg] opacity-5"/>
-    <Polygon size={368} className="circle absolute top-[1850px] right-[124px] opacity-5"/>
-    <Polygon className="pentagon absolute top-[1540px] left-1/3 -rotate-[15deg] opacity-5"/>
-    <Polygon size={57} className="circle absolute top-[1578px] left-1/3 opacity-5"/>
-
-    <Polygon size={331} className="blur-circle absolute -top-[150px] -left-[150px]"/>
-    <Polygon size={190} className="blur-circle absolute -top-[61px] right-[165px] hidden lg:block"/>
-    <Polygon size={230} className="blur-circle absolute top-1/2 -right-[131px]"/>
-    <Polygon size={145} className="blur-circle absolute top-1/2 left-0"/>
-    <Polygon size={343} className="blur-circle absolute top-1/2 left-1/2 hidden lg:block"/>
-    <Polygon size={332} className="blur-circle absolute -bottom-[160px] left-1/2"/>
+    <Polygon
+      size={300}
+      className="triangle absolute top-0 right-0 rotate-[165deg] opacity-5"
+    />
+    <Polygon
+      size={380}
+      className="triangle absolute top-[950px] -left-10 rotate-[195deg] opacity-5"
+    />
+    <Polygon
+      size={100}
+      className="square absolute top-0 right-6 rotate-[60deg] opacity-5"
+    />
+    <Polygon
+      size={87}
+      className="square absolute top-[2200px] left-10 rotate-[30deg] opacity-5"
+    />
+    <Polygon
+      size={368}
+      className="circle absolute top-[1850px] right-[124px] opacity-5"
+    />
+    <Polygon className="pentagon absolute top-[1540px] left-1/3 -rotate-[15deg] opacity-5" />
+    <Polygon
+      size={57}
+      className="circle absolute top-[1578px] left-1/3 opacity-5"
+    />
   </div>
-)
+);
 
-export default function Background({children}) {
+const Light = () => {
+  return (
+    <>
+      <div className="fixed -top-20 right-0 bg-white w-80 h-80 opacity-10 lg:opacity-60 rounded-full blur-3xl backdrop-blur-3xl" />
+      <div className="fixed -top-20 left-0 bg-white w-60 h-80 opacity-30 lg:opacity-50 rounded-full blur-3xl backdrop-blur-3xl" />
+      <div className="fixed -bottom-10 left-1/2 -translate-x-1/2 bg-white w-[500px] h-60 opacity-20 lg:opacity-60 rounded-full blur-3xl backdrop-blur-3xl" />
+    </>
+  );
+};
+
+export default function Background({ children }) {
   return (
     <div className="background relative">
+      <Light />
       <Polygons />
       <main>{children}</main>
       <div className="fixed bottom-0 md:top-1/2 right-5 z-10">
--- apps/dr-bill/components/Polygon.tsx
+++ apps/dr-bill/components/Polygon.tsx
@@ -1,67 +1,61 @@
 import { useTheme } from '@nextui-org/react';
-import BlurImage from '../public/assets/blur.png';
 
 type Props = {
   id?: string;
   size?: number;
   className?: string;
-}
+};
 
-export default function Polygon({size, className}: Props) {
-  const {theme} = useTheme();
+export default function Polygon({ size, className }: Props) {
+  const { theme } = useTheme();
   const darkColor = theme.colors.accents9.value;
   return (
     <div className={className}>
-      <style jsx>{`
-        .circle, .square, .blur-circle {
-          width: ${size}px;
-          height: ${size}px;
-          background: ${darkColor};
-        }
+      <style jsx>
+        {`
+          .circle,
+          .square,
+          .blur-circle {
+            width: ${size}px;
+            height: ${size}px;
+            background: ${darkColor};
+          }
 
-        .circle {
-          -moz-border-radius: 50%;
-          -webkit-border-radius: 50%;
-          border-radius: 50%;
-        }
+          .circle {
+            -moz-border-radius: 50%;
+            -webkit-border-radius: 50%;
+            border-radius: 50%;
+          }
 
-        .blur-circle {
-          background-color: transparent;
-          background-image: url(${BlurImage.src});
-          background-position: center;
-          background-repeat: no-repeat;
-          background-size: contain;
-        }
+          .triangle {
+            width: 0;
+            height: 0;
+            border-bottom: ${size * 0.866}px solid ${darkColor};
+            border-left: ${size * 0.5}px solid transparent;
+            border-right: ${size * 0.5}px solid transparent;
+          }
 
-        .triangle {
-          width: 0;
-          height: 0;
-          border-bottom: ${size * 0.866}px solid ${darkColor};
-          border-left: ${size * 0.5}px solid transparent;
-          border-right: ${size * 0.5}px solid transparent;
-        }
+          .pentagon {
+            position: relative;
+            width: 150px;
+            box-sizing: content-box;
+            border-width: 139px 50px 0;
+            border-style: solid;
+            border-color: ${darkColor} transparent;
+          }
 
-        .pentagon {
-          position: relative;
-          width: 150px;
-          box-sizing: content-box;
-          border-width: 139px 50px 0;
-          border-style: solid;
-          border-color: ${darkColor} transparent;
-        }
-
-        .pentagon:before {
-          content: "";
-          position: absolute;
-          height: 0;
-          width: 0;
-          top: -236px;
-          left: -50px;
-          border-width: 0 125px 97px;
-          border-style: solid;
-          border-color: transparent transparent ${darkColor};
-        }
-      `}
+          .pentagon:before {
+            content: '';
+            position: absolute;
+            height: 0;
+            width: 0;
+            top: -236px;
+            left: -50px;
+            border-width: 0 125px 97px;
+            border-style: solid;
+            border-color: transparent transparent ${darkColor};
+          }
+        `}
       </style>
     </div>
   );
